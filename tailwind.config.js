d679ea60d0815b14c47ea6f6687455a387ad70c1
Badar Wildani <wildanie12@gmail.com>
2022-08-13 05:42:24 +0700

    feat: tailwindcss ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './views/**/*.{html,ghtml}'
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: 'Poppins'
+      }
+    },
   },
   plugins: [],
 }

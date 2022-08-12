13e11944925b6ee1c55ad4d45aeb457e792f79c6
Badar Wildani <wildanie12@gmail.com>
2022-08-13 05:42:24 +0700

    feat: added store task feature

--- controllers/task.controller.go
+++ controllers/task.controller.go
@@ -3,6 +3,8 @@ package controllers
 import (
 	"fmt"
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/labstack/echo/v4"
 	_models "github.com/wildanie12/dts-task-planner/models"
@@ -29,4 +31,29 @@ func (controller TaskController) Index(c echo.Context) error  {
 	return c.Render(http.StatusOK, "worker-index.html", map[string]interface{} {
 		"tasks": tasks,
 	})
+}
+
+func (controller TaskController) Store(c echo.Context) error  {
+
+	taskRequest := _models.Task{}
+	c.Bind(&taskRequest)
+
+	// Populate data 
+	startDate, _ := time.Parse("2006-01-02", c.FormValue("start_date"))
+	taskRequest.StartDate = startDate
+
+	endDate, _ := time.Parse("2006-01-02", c.FormValue("end_date"))
+	taskRequest.EndDate = endDate
+
+	workerID, _ := strconv.Atoi(c.FormValue("worker_id"))
+	taskRequest.WorkerID = uint(workerID)
+	
+	done, _ := strconv.ParseBool(c.FormValue("done"))
+	taskRequest.Done = done
+
+	fmt.Println(taskRequest)
+
+	controller.db.Save(&taskRequest)
+
+	return c.Redirect(http.StatusMovedPermanently, "/tasks")
 }
